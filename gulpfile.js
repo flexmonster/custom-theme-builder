991111536f10ae0d8f17763eac4781bb7963d604
Diachenko Maksym <52499153+maksymkaD@users.noreply.github.com>
2023-01-18 14:13:07 +0200

    Updated dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,16 @@
 const gulp = require('gulp');
-const gutil = require('gulp-util');
 const replace = require('gulp-replace');
 const notify = require('gulp-notify');
 const less = require('gulp-less');
 const cleanCSS = require('gulp-clean-css');
-//const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename');
+const log = require('fancy-log');
 
 const destination = 'generated-theme/';
 
 gulp.task('compile-css', function () {
     return gulp.src('flexmonster.less')
-        //.pipe(sourcemaps.init())
-        .pipe(less().on('error', gutil.log))
-        //.pipe(sourcemaps.write())
+        .pipe(less().on('error', log))
         .pipe(gulp.dest(destination));
 });
 
@@ -27,13 +24,10 @@ gulp.task('adjust-assets', function(){
 
 gulp.task('minify-css',() => {
   return gulp.src(destination+'flexmonster.css')
-    //.pipe(sourcemaps.init())
     .pipe(cleanCSS())
     .pipe(rename({ suffix: ".min" }))
-    //.pipe(sourcemaps.write())
     .pipe(gulp.dest(destination))
     .pipe(notify('Generated theme in '+destination+' folder'));
 });
 
 gulp.task('default', gulp.series(['compile-css', 'adjust-assets', 'minify-css']));
-
